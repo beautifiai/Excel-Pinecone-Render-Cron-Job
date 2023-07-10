ab270880b8a716b1e199f12bdc5fba5e16e3e100
Lachlan F <137961538+beautifiai@users.noreply.github.com>
2023-07-10 14:24:00 -0700

    Update pinecone_handler.py

--- pinecone_handler.py
+++ pinecone_handler.py
@@ -1,33 +1,20 @@
-import os
 import pinecone
+import os
 
 class PineconeHandler:
-    def __init__(self):
-        self.pinecone_key = os.getenv('PINECONE_KEY')
-
-        # Initialize Pinecone
-        pinecone.init(api_key=self.pinecone_key)
+    def __init__(self, pinecone_key):
+        pinecone.deinit()
+        pinecone.init(api_key=pinecone_key)
+        self.index_name = "my-vector-index"
+        if self.index_name not in pinecone.list_indexes():
+            pinecone.create_index(name=self.index_name, metric="cosine", shards=1)
+        self.index = pinecone.Index(index_name=self.index_name)
 
     def insert_document(self, namespace, embedding):
-        """
-        Inserts a new document into the Pinecone database.
-        """
-        # Initialize the index with the namespace if it doesn't exist
-        if namespace not in pinecone.list_indexes():
-            pinecone.create_index(index_name=namespace, metric="cosine")
-
-        # Switch to the namespace and insert the document
-        pinecone.deactivate()
-        pinecone.activate(index_name=namespace)
-        pinecone.upsert(items={namespace: embedding})
+        self.index.upsert(items={namespace: embedding})
 
     def remove_document(self, namespace):
-        """
-        Removes a document from the Pinecone database.
-        """
-        # Check if the namespace exists
-        if namespace in pinecone.list_indexes():
-            # Switch to the namespace and delete the document
-            pinecone.deactivate()
-            pinecone.activate(index_name=namespace)
-            pinecone.delete(ids=[namespace])
+        self.index.delete(items=[namespace])
+
+    def __del__(self):
+        pinecone.deinit()
