c8776c1fe8b66d6b0afaaa35803243c9e9fb24ff
Lachlan F <137961538+beautifiai@users.noreply.github.com>
2023-07-10 13:31:32 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from excel_handler import ExcelHandler
+from pinecone_handler import PineconeHandler
+from github_handler import GithubHandler
+from embedding_handler import EmbeddingHandler
+
+def main():
+    # Initialize handlers
+    excel_handler = ExcelHandler()
+    pinecone_handler = PineconeHandler()
+    github_handler = GithubHandler()
+    embedding_handler = EmbeddingHandler()
+
+    # Get current and previous Excel data
+    current_data = excel_handler.get_excel_data()
+    previous_data = github_handler.get_previous_data()
+
+    # Determine rows to add and remove
+    to_add, to_remove = excel_handler.compare_data(current_data, previous_data)
+
+    # Update Pinecone database
+    for row in to_add:
+        namespace, embedding_data = row[0], row[1:]
+        embedding = embedding_handler.create_embedding(embedding_data)
+        pinecone_handler.insert_document(namespace, embedding)
+    for namespace in to_remove:
+        pinecone_handler.remove_document(namespace)
+
+    # Update Excel file in repository
+    github_handler.update_file(current_data)
+
+if __name__ == "__main__":
+    main()
