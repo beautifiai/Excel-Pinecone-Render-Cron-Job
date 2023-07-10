146a82b4808ca549f766ca8c2298b1351dd923e5
Lachlan F <137961538+beautifiai@users.noreply.github.com>
2023-07-10 14:25:09 -0700

    Update excel_handler.py

--- excel_handler.py
+++ excel_handler.py
@@ -1,25 +1,21 @@
 import pandas as pd
-from openpyxl import load_workbook
 
 class ExcelHandler:
     def __init__(self, excel_url):
         self.excel_url = excel_url
-
+    
     def get_excel_data(self):
-        """
-        Reads the Excel data from the provided URL and returns a dictionary with namespaces as keys
-        and rows (as lists) as values.
-        """
-        data = pd.read_excel(self.excel_url, header=None)
-        data_dict = data.set_index(0).T.to_dict('list')
-        return data_dict
+        data = pd.read_excel(self.excel_url)
+        return data
 
     def compare_data(self, current_data, previous_data):
-        """
-        Compares the current and previous data and returns two lists:
-        - 'to_add': contains rows present in current_data but not in previous_data
-        - 'to_remove': contains namespaces present in previous_data but not in current_data
-        """
-        to_add = {namespace: row for namespace, row in current_data.items() if namespace not in previous_data}
-        to_remove = [namespace for namespace in previous_data if namespace not in current_data]
+        current_namespaces = set(current_data.iloc[:, 0])
+        previous_namespaces = set(previous_data.iloc[:, 0])
+
+        namespaces_to_add = current_namespaces - previous_namespaces
+        namespaces_to_remove = previous_namespaces - current_namespaces
+
+        to_add = current_data[current_data.iloc[:, 0].isin(namespaces_to_add)]
+        to_remove = previous_data[previous_data.iloc[:, 0].isin(namespaces_to_remove)]
+
         return to_add, to_remove
