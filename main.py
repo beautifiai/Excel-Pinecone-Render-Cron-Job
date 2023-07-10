b575a9a52508c40d619fd8785043e4e607bd4a3f
Lachlan F <137961538+beautifiai@users.noreply.github.com>
2023-07-10 14:18:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from github_handler import GithubHandler
 from embedding_handler import EmbeddingHandler
 
 def main():
+    # Obtain necessary keys and URLs from environment variables
+    excel_url = os.getenv('EXCEL_URL')
+    pinecone_key = os.getenv('PINECONE_KEY')
+    openai_key = os.getenv('OPENAI_KEY')
+    github_token = os.getenv('GITHUB_TOKEN')
+
     # Initialize handlers
-    excel_handler = ExcelHandler()
-    pinecone_handler = PineconeHandler()
-    github_handler = GithubHandler()
-    embedding_handler = EmbeddingHandler()
+    excel_handler = ExcelHandler(excel_url)
+    pinecone_handler = PineconeHandler(pinecone_key)
+    github_handler = GithubHandler(github_token, "beautifiai", "Excel-Scraper")
+    embedding_handler = EmbeddingHandler(openai_key)
 
     # Get current and previous Excel data
     current_data = excel_handler.get_excel_data()
@@ -23,6 +29,7 @@ def main():
         namespace, embedding_data = row[0], row[1:]
         embedding = embedding_handler.create_embedding(embedding_data)
         pinecone_handler.insert_document(namespace, embedding)
+
     for namespace in to_remove:
         pinecone_handler.remove_document(namespace)
 
