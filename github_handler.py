b19c1683f5859726df5487f73297d3d2d7eb40f0
Lachlan F <137961538+beautifiai@users.noreply.github.com>
2023-07-10 14:25:40 -0700

    Update github_handler.py

--- github_handler.py
+++ github_handler.py
@@ -1,38 +1,20 @@
-import os
 from github import Github
 import base64
 
 class GithubHandler:
-    def __init__(self, repository_name):
-        self.github_token = os.getenv('GITHUB_TOKEN')
-        self.repository_name = repository_name
-
-        # Initialize GitHub client
-        self.client = Github(self.github_token)
-        self.repo = self.client.get_user().get_repo(self.repository_name)
+    def __init__(self, repo_name, token):
+        self.github = Github(token)
+        self.repo = self.github.get_user().get_repo(repo_name)
+        self.file_path = "data.xlsx"
 
     def get_previous_data(self):
-        """
-        Fetches the previous version of the Excel file from the GitHub repository,
-        decodes it, and returns it as a pandas DataFrame.
-        """
-        contents = self.repo.get_contents("data.xlsx")
-        decoded_contents = base64.b64decode(contents.content)
-        data = pd.read_excel(io.BytesIO(decoded_contents), header=None)
-        data_dict = data.set_index(0).T.to_dict('list')
-        return data_dict
+        file_content = self.repo.get_contents(self.file_path)
+        file_data = base64.b64decode(file_content.content)
+        return pd.read_excel(io.BytesIO(file_data))
 
-    def update_file(self, data):
-        """
-        Writes the updated data to the Excel file and pushes it to the GitHub repository.
-        """
-        # Write data to Excel file
-        df = pd.DataFrame.from_dict(data, orient='index')
-        df.to_excel('data.xlsx', index=False)
-
-        # Read the updated file as bytes
-        with open('data.xlsx', 'rb') as file:
+    def update_file(self, current_data):
+        current_data.to_excel(self.file_path, index=False)
+        with open(self.file_path, 'rb') as file:
             content = file.read()
+        self.repo.update_file(self.file_path, "Update data", content, self.file_path)
 
-        # Update the file in the repository
-        self.repo.update_file("data.xlsx", "Update data", content, self.repo.get_contents("data.xlsx").sha)
